998383d50d8868a6dcc97264dc3fad597121564a
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-22 16:23:33 +0300

    adding a press eventlistener

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,32 @@
-cont 
+const search = document.getElementById('search')
+const city =document.getElementById('search-city')
+city.addEventListener('keypress',searchCity)
+
+search.addEventListener("click",searchCity)
 
 
+function searchCity(event){
+    // event.preventDefault()
+    if (event.keyCode == 13){
+        // displayData(city.value)
+        console.log(city.value )
+    }
+
+}
+
+// function displayData(){
 
 
+// }
 
-
-
-
-
-a
 
 //Get request
-function fecthData(){
-    let url ='https://api.openweathermap.org/data/2.5/weather?q=Machakos,Kenya&APPID=e3dc067684488394b79ea9c31d805834'
-    fetch(url)
-    .then(response =>response.json())
-    .then(data=>{
-        console.log(data)
-    })
-}
-fecthData()
+// function fecthData(){
+//     let url ='https://api.openweathermap.org/data/2.5/weather?q=Machakos,Kenya&APPID=e3dc067684488394b79ea9c31d805834'
+//     fetch(url)
+//     .then(response =>response.json())
+//     .then(data=>{
+//         console.log(data)
+//     })
+// }
+// fecthData()
