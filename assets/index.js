d36e463796b83d9ebc91474fb94ba31239fdb6c2
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-23 07:54:43 +0300

    modified index.html to add a button to give more information

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,30 @@
 //selection
+document.addEventListener('DOMContentedLoaded',()=>{
+    fecthData()
+})
 const search = document.getElementById('search')
 const inputCity =document.getElementById('search-city')
 let locate =document.querySelector('.location')
 let weatherDescription =document.querySelector('#weather')
 let weatherTemperature=document.querySelector('#temperature')
 let weatherHimidity=document.querySelector('#humidity')
+let weatherIcon =document.querySelector('#icon')
+
 
 
 //eventlisteners
 search.addEventListener("click",buttonClick)
 inputCity.addEventListener('keypress',searchCity)  
+
 //entlisteners functions
+//using the search button
 function buttonClick(event){
 event.preventDefault()
 fecthData()
 }
+//using the Enter button to get the input
 function searchCity(event){
-    // event.preventDefault()
+
     if (event.keyCode == 13){
         fecthData()
         
@@ -24,9 +32,13 @@ function searchCity(event){
 
 }
 
+
+//Dispay fetched information
+
 function displayData(data){
 locate.innerHTML =`${data.name},${data.sys.country}` 
-weatherDescription.innerHTML =`${data.weather[0].description},${data.weather[0].icon}`
+weatherDescription.innerHTML =`${data.weather[0].description}`
+weatherIcon.src=`https//openweathermap.org/img/wn/"${data.weather[0].icon}"@2x.png`
 weatherTemperature.innerHTML =`Temperature:&nbsp&nbsp ${data.main.temp}<span>°C</span>`
 weatherHimidity.innerHTML= `Humidity:&nbsp&nbsp ${data.main.humidity}<span>%</span>`
 
