b3adc4a494884e9b54a4e5ea7cd8910eba6640b3
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-23 05:47:53 +0300

    fetch humidity results

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,7 @@ const inputCity =document.getElementById('search-city')
 let locate =document.querySelector('.location')
 let weatherDescription =document.querySelector('#weather')
 let weatherTemperature=document.querySelector('#temperature')
+let weatherHimidity=document.querySelector('#humidity')
 
 
 //eventlisteners
@@ -26,13 +27,15 @@ function searchCity(event){
 function displayData(data){
 locate.innerHTML =`${data.name},${data.sys.country}` 
 weatherDescription.innerHTML =`${data.weather[0].description},${data.weather[0].icon}`
-weatherTemperature.innerHTML =`${data.main.temp}`
+weatherTemperature.innerHTML =`Temperature:&nbsp&nbsp ${data.main.temp}<span>°C</span>`
+weatherHimidity.innerHTML= `Humidity:&nbsp&nbsp ${data.main.humidity}<span>%</span>`
+
 }
 
 
 //Get request
 function fecthData(){
-    let url ='https://api.openweathermap.org/data/2.5/weather?q='+inputCity.value+'&APPID=e3dc067684488394b79ea9c31d805834'
+    let url ='https://api.openweathermap.org/data/2.5/weather?q='+inputCity.value+'&units=metric&APPID=e3dc067684488394b79ea9c31d805834'
     fetch(url)
     .then(response =>response.json())
     .then(data=>{
