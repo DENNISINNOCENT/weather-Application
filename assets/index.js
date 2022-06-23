6e426b725f251bb273cb69c283e713e0ed7c0419
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-23 10:52:17 +0300

    selection of elements and fetching timezone data

--- assets/index.js
+++ assets/index.js
@@ -11,17 +11,21 @@ let weatherHimidity=document.querySelector('#humidity')
 let weatherIcon =document.querySelector('#icon')
 const morebtn =document.querySelector('#more')
 const hiddeninfo =document.querySelector('.hidden')
+const weatherTimezone=document.getElementById('timezone')
+const weatherPressure=document.getElementById('pressure')
+const weatherWind=document.getElementById('wind')
+const locationCoordinates=document.getElementById('coordinates')
 
 
 
 //eventlisteners
-search.addEventListener("click",buttonClick)
+search.addEventListener("click",buttonSubmit)
 inputCity.addEventListener('keypress',searchCity) 
 morebtn.addEventListener('click',showInfo) 
 
 //entlisteners functions
 //using the search button
-function buttonClick(event){
+function buttonSubmit(event){
 event.preventDefault()
 fecthData()
 }
@@ -46,15 +50,17 @@ function showInfo(){
 function displayData(data){
 locate.innerHTML =`${data.name},${data.sys.country}` 
 weatherDescription.innerHTML =`${data.weather[0].description}`
-weatherIcon.src=`https//openweathermap.org/img/wn/"${data.weather[0].icon}"@2x.png`
+// weatherIcon.src=`https//openweathermap.org/img/wn/"${data.weather[0].icon}"@2x.png`
 weatherTemperature.innerHTML =`Temperature:&nbsp&nbsp ${data.main.temp}<span>°C</span>`
-weatherHimidity.innerHTML= `Humidity:&nbsp&nbsp ${data.main.humidity}<span>%</span>`
+weatherHimidity.innerHTML= `Humidity:&nbsp ${data.main.humidity}<span>%</span>`
+weatherTimezone.innerHTML=`Timezone:&nbsp&nbsp&${data.timezone}`
+weatherPressure.innerHTML=``
 
 }
 //reload the page after every 30seconds
 setTimeout(() =>{
     location.reload();
-  }, 30000);
+  }, 60000);
 
 
 //Get request
@@ -65,6 +71,9 @@ function fecthData(){
     .then(data=>{
       displayData(data)
       console.log(data)
+    // .catch(error=>{
+    //   console.log(error)
+    // })
     })
 }
 
